32aed9a076e80146ac845d765258178cfe8d2968
Maryam.as <203218399+Maryam-as@users.noreply.github.com>
2025-03-14 23:32:41 +0330

    output dynamic react descriptions in Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+// define a list of words
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+// generate random index to access these words randomly
+function generateRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
+  // to keep JSX code leaner
+  const description = reactDescriptions[generateRandomInt(2)];
+
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
