381eccfba420b8c0e51ccdc2aa3e22b3e158ad17
Maryam.as <203218399+Maryam-as@users.noreply.github.com>
2025-03-15 12:36:07 +0330

    pass props to CoreConcept function

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ function Header() {
   );
 }
 
-function CoreConcept() {
+function CoreConcept(props) {
   return (
     <li>
-      <img src="..." alt="..." />
-      <h3>TITLE</h3>
-      <p>DESCRIPTION</p>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
     </li>
   );
 }
