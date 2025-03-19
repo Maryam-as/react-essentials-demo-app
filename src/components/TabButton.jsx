2656343572fa4e4db0fe9ba8ff1b878c5dd63f5e
Maryam.as <203218399+Maryam-as@users.noreply.github.com>
2025-03-19 23:11:00 +0330

    set the active className on the buttons dynamically

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,9 +1,12 @@
 // object destructuring
-export default function TabButton({ children, onSelect }) {
+export default function TabButton({ children, onSelect, isSelected }) {
   return (
     <li>
       {/* handleClick must not be executed, written without () */}
-      <button onClick={onSelect}>{children}</button>
+      {/* add the active class if the button is selected */}
+      <button className={isSelected ? "active" : undefined} onClick={onSelect}>
+        {children}
+      </button>
     </li>
   );
 }
